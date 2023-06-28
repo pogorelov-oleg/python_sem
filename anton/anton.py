6fa7a7214eb4b6b96cd666277b2f94a052ad4047
pogorelov <89607888555@mail.ru>
2023-06-29 02:16:53 +0700

    anton done

---
+++ anton/anton.py
@@ -0,0 +1,18 @@
+string = {"1": "222anton456", "2": "a1n1t1o1n1", "3": "0000a0000n00t00000o000000n", "4": "gylfole", "5": "richard", "6": "ant0n"}              
+
+def Check_Virus(string, virus):
+    check = ""
+    for i in virus:
+        for j in string:
+            if i == j:
+                check += i
+                break
+    if check == virus:
+        return("Virus")
+    else:
+        return("Ok")  
+
+for i in string:
+    print(f'{i} - {Check_Virus(string[i], "anton")}')
+ 
+
