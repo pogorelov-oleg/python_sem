8310a305363451d49a71e07d2a3e3fc3fe02e32c
pogorelov <89607888555@mail.ru>
2023-06-29 12:44:17 +0700

    anton edit_01

--- anton/anton.py
+++ anton/anton.py
@@ -1,18 +1,16 @@
 string = {"1": "222anton456", "2": "a1n1t1o1n1", "3": "0000a0000n00t00000o000000n", "4": "gylfole", "5": "richard", "6": "ant0n"}              
 
 def Check_Virus(string, virus):
-    check = ""
     for i in virus:
-        for j in string:
-            if i == j:
-                check += i
-                break
-    if check == virus:
-        return("Virus")
-    else:
-        return("Ok")  
+            if i not in string:
+                return("Ok")
+            else:
+                string = string[string.index(i):]
+    return("Virus")    
+          
 
-for i in string:
+for i in string: 
     print(f'{i} - {Check_Virus(string[i], "anton")}')
- 
+
+
 
